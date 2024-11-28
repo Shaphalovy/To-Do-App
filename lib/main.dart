39b5a8e1705184b5a07e18886e35f72fc96cc5bb
Sabbir <paholoby@gmail.com>
2024-11-28 20:11:07 +0600

    Second Commit - Added Task Editing and Removing Features

--- lib/main.dart
+++ lib/main.dart
@@ -41,52 +41,131 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
             child: ListTile(
               title: Text(tasks[index]['title']!),
               subtitle: Text(tasks[index]['description']!),
-            ),);
+              trailing: IconButton(onPressed: (){
+                setState(() {
+                  tasks.removeAt(index);
+                });
+              }, icon: Icon(Icons.delete), color: Colors.red,),
+              onTap: () {
+                _editTaskDialog(index);
+              },
 
+            ),
+          );
         },
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        showDialog(context: context, builder: (BuildContext context){
-          String newTaskTitle = "";
-          String newDescription = "";
-          return  AlertDialog(
-            title: const Text("Add a new Task"),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextField(onChanged: (value){
-                  newTaskTitle = value;
-                },
-                decoration: InputDecoration(hintText: "Enter a task name"),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              String newTaskTitle = "";
+              String newDescription = "";
+              return AlertDialog(
+                title: const Text("Add a new Task"),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      onChanged: (value) {
+                        newTaskTitle = value;
+                      },
+                      decoration: InputDecoration(hintText: "Enter a task name"),
+                    ),
+                    TextField(
+                      onChanged: (value) {
+                        newDescription = value;
+                      },
+                      decoration:
+                      InputDecoration(hintText: "Description of the task"),
+                    ),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Cancel"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      if (newTaskTitle.isNotEmpty &&
+                          newDescription.isNotEmpty) {
+                        setState(() {
+                          tasks.add({
+                            'title': newTaskTitle,
+                            'description': newDescription,
+                          });
+                        });
+                      }
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Add"),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add, color: Colors.orangeAccent),
+      ),
+    );
+  }
 
-                TextField(onChanged: (value){
-                  newDescription = value;
-                },
-                decoration: InputDecoration(hintText: "Description of the task"),),
-              ],
-            ),
-            actions: [
-              TextButton(onPressed: (){
+  void _editTaskDialog(int index) {
+    // Initialize TextEditingControllers
+    TextEditingController titleController =
+    TextEditingController(text: tasks[index]['title']);
+    TextEditingController descriptionController =
+    TextEditingController(text: tasks[index]['description']);
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Edit your task!"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: titleController,
+                decoration: const InputDecoration(hintText: "Task title"),
+              ),
+              TextField(
+                controller: descriptionController,
+                decoration: const InputDecoration(hintText: "Task description"),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
                 Navigator.of(context).pop();
-              }, child: const Text("Cancel")),
-              TextButton(onPressed: (){
-                if(newTaskTitle.isNotEmpty && newDescription.isNotEmpty){
-                  setState(() {
-                    tasks.add({
-                      'title': newTaskTitle,
-                      'description': newDescription,
-                    });
+              },
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                String updatedTitle = titleController.text;
+                String updatedDescription = descriptionController.text;
 
+                if (updatedTitle.isNotEmpty &&
+                    updatedDescription.isNotEmpty) {
+                  setState(() {
+                    tasks[index] = {
+                      'title': updatedTitle,
+                      'description': updatedDescription,
+                    };
                   });
                 }
                 Navigator.of(context).pop();
-              }, child: const Text("Add"))
-            ],
-
-          );
-        });
-      },child: Icon(Icons.add, color: Colors.orangeAccent),),
-
+              },
+              child: const Text("Save"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
