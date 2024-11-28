1254fd2489cae2aa19bc7079439072761c0c4b4e
Sabbir <paholoby@gmail.com>
2024-11-28 15:03:11 +0600

    Initial Commit-Only Add Task option added in this version

---
+++ README.md
@@ -0,0 +1,21 @@
+# todo_list_app
+
+A simple To Do List App. 
+Key Features:
+Add, edit, and delete tasks.
+Mark tasks as completed.
+Organize tasks by categories.
+Save data locally using SQLite or shared preferences.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ToDoListScreen(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
+
+class ToDoListScreen extends StatefulWidget {
+  @override
+  _ToDoListScreenState createState() => _ToDoListScreenState();
+}
+
+List<Map<String, String>> tasks = [];
+
+class _ToDoListScreenState extends State<ToDoListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("To-Do List"),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 5,
+            shadowColor: Colors.orangeAccent,
+            child: ListTile(title: Text(tasks[index]['title']!),
+              subtitle: Text(tasks[index]['description']!),),);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        showDialog(context: context, builder: (BuildContext context){
+          String newTaskTitle = "";
+          String newDescription = "";
+          return  AlertDialog(
+            title: const Text("Add a new Task"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(onChanged: (value){
+                  newTaskTitle = value;
+                },
+                decoration: InputDecoration(hintText: "Enter a task name"),),
+
+                TextField(onChanged: (value){
+                  newDescription = value;
+                },
+                decoration: InputDecoration(hintText: "Description of the task"),),
+              ],
+            ),
+            actions: [
+              TextButton(onPressed: (){
+                Navigator.of(context).pop();
+              }, child: const Text("Cancel")),
+              TextButton(onPressed: (){
+                if(newTaskTitle.isNotEmpty && newDescription.isNotEmpty){
+                  setState(() {
+                    tasks.add({
+                      'title': newTaskTitle,
+                      'description': newDescription,
+                    });
+
+                  });
+                }
+                Navigator.of(context).pop();
+              }, child: const Text("Add"))
+            ],
+
+          );
+        });
+      },child: Icon(Icons.add, color: Colors.orangeAccent),),
+    );
+  }
+}
